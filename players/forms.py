56d8abf279f996a7b946a006b91f9e0882a59f4e
Jason Shive <jwshive@gmail.com>
2017-03-29 14:45:01 -0400

    Allowing bnet tags as usernames.

--- players/forms.py
+++ players/forms.py
@@ -1,18 +1,5 @@
 from django import forms
 from players.models import Characters
-from django.contrib.auth.models import User
-from django.contrib.auth.validators import UnicodeUsernameValidator
-
-
-class MyValidator(UnicodeUsernameValidator):
-    regex = r"^[\w.]+#\d+$"
-
-
-class MyUser(User):
-    username_validator = MyValidator
-
-    class Meta:
-        proxy = True  # If no new field is added.
 
 
 class AddCharacterForm(forms.ModelForm):
