639a027d1ceed60848c1d516ab5fcd887dd310f6
Jason Shive <jwshive@gmail.com>
2017-03-29 14:43:17 -0400

    Allowing bnet tags as usernames.

--- players/admin.py
+++ players/admin.py
@@ -1,30 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import forms
-from django import forms
 from .models import Characters, CharactersDetails, Classes, Factions, Realms, Races, Timezones, Regions, Profile
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-username_regex_field = forms.RegexField(
-    label="Username",
-    max_length=30,
-    regex=r"^[\w.]+#\d+$",
-    help_text="Required. 30 characters or fewer. Letters, apostrophes, periods, hyphens and at signs.",
-    error_message="This value must contain only letters, apostrophes, periods, hyphens and at signs."
-)
-
-
-class UserCreationForm(UserCreationForm):
-    username = username_regex_field
-
-
-class UserChangeForm(UserChangeForm):
-    username = username_regex_field
-
-
-class UserProfileAdmin(UserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
 
 
 class RaceAdmin(admin.ModelAdmin):
