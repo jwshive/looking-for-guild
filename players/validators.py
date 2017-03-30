07aa6e37cbcccfa5233fd790b2b8eabd5f8ae618
Jason Shive <jwshive@gmail.com>
2017-03-29 21:21:22 -0400

    Upgraded django-allauth

--- players/validators.py
+++ players/validators.py
@@ -1,21 +1,3 @@
-import re
+from allauth.socialaccount.providers.battlenet import validators
 
-from django.core.exceptions import ValidationError
-from django.utils.deconstruct import deconstructible
-from django.utils.translation import ugettext_lazy as _
-
-
-@deconstructible
-class CustomUsernameValidator(object):
-    message = _('Invalid username')
-
-    def __call__(self, value):
-        if not re.match(r"^[\w.]+#\d+$", value):
-            raise ValidationError(self.message, code='invalid_username')
-
-
-def username_validators():
-    from allauth.socialaccount.providers.battlenet import validators
-    return [
-        validators.BattletagUsernameValidator
-    ]
+validators_list = [ validators.BattletagUsernameValidator, ]
