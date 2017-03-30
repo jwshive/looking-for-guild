2b6cbf5fd69c7092e5870856cf1bfa4c2eb72b8b
Jason Shive <jwshive@gmail.com>
2017-03-29 20:34:10 -0400

    Trying this again.

---
+++ players/validators.py
@@ -0,0 +1,14 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import ugettext_lazy as _
+
+
+@deconstructible
+class CustomUsernameValidator(object):
+    message = _('Invalid username')
+
+    def __call__(self, value):
+        if not re.match(r"^[\w.]+#\d+$", value):
+            raise ValidationError(self.message, code='invalid_username')
