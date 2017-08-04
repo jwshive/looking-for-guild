6ea4995652b2d4eb87f9bb839305ee14bcd6f72c
Jason Shive <jshive@iot.in.gov>
2017-08-04 11:39:18 -0400

    Updated for the bnet oauth

--- LookingForGuild/settings.py
+++ LookingForGuild/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'runtime_static')
 
-SITE_ID = 3
+SITE_ID = 2
 
 redirect_uri = '/players/profile'
 LOGIN_REDIRECT_URL = redirect_uri
