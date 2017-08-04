770366055fc00f2b5afce3463e4f00adbb73b87b
Jason Shive <jshive@iot.in.gov>
2017-08-04 11:32:00 -0400

    Update db for heroku

--- LookingForGuild/settings.py
+++ LookingForGuild/settings.py
@@ -82,16 +82,10 @@ WSGI_APPLICATION = 'LookingForGuild.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'looking_for_guild',
-            'USER': 'lfg_site_user_name',
-            'PASSWORD': 'qqtr553N!',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-            }
-}
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
